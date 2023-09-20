50a760ce9df3915d33c5fdf758c9c7506d837479
Yordanos Tibebu <yordanostibebu5@gmail.com>
2023-09-20 04:00:04 -0700

    initial commit

---
+++ about.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <section class="header">
+
+      <a href="home.php" class="logo">YMS Tour and Travel</a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="package.php">Tour site</a>
+         <a href="book.php">Reservation</a>
+         <a href="about.php">about</a>
+         <a href="admin.php">Agent</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   <!-- header section ends -->
+
+   <div class="heading_1" style="background:url(images/header.jpg) no-repeat">
+      <h1>about us</h1>
+   </div>
+
+   <!-- about section starts  -->
+
+   <section class="about">
+
+      <div class="content">
+         <h3>why YMS ?</h3>
+         <p>Welcome to our tour and travel website! We are dedicated to providing you with the best travel experiences and making your trips unforgettable.</p>
+         <p> Here's why you should choose us:</p>
+
+         <ol type="1">
+            <li>
+               <p> Expertise: Our team of travel experts has years of experience in the industry and a deep knowledge of destinations around the world. We are passionate about travel and committed to finding you the perfect itinerary to match your interests and budget.</p>
+            </li>
+
+            <li>
+               <p>Personalized Service: We understand that every traveler is unique, and we work closely with you to create a customized travel plan that suits your individual needs. From booking flights and accommodations to arranging activities and excursions, we take care of all the details so you can relax and enjoy your trip.</p>
+            </li>
+
+            <li>
+               <p>Affordable Options: We believe that everyone should have the opportunity to travel, which is why we offer a wide range of affordable travel packages and options. We are committed to finding the best deals and discounts for you, so you can get the most out of your budget.</p>
+            </li>
+
+            <li>
+               <p>Unforgettable Experiences: Whether you're looking for adventure, cultural immersion, or simply some relaxation, we have something for everyone. Our tours and activities are designed to provide you with unforgettable experiences and memories that will last a lifetime.</p>
+            </li>
+
+            <p>At our tour and travel website, our goal is to make your travel dreams a reality. Contact us today to start planning your next adventure!"</p>
+         </ol>
+
+      </div>
+
+   </section>
+
+   <!-- about section ends -->
+
+   <!-- reviews section starts  -->
+
+
+
+   <!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> Tour site</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i>Reservation</a>
+            <a href="admin.php"><i class="fas fa-angle-right"></i> Agent</a>
+         </div>
+
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +251-988209404</a>
+            <a href="#"> <i class="fas fa-phone"></i> +251-910133245 </a>
+            <a href="YMStourandtravel@gmail.com"> <i class="fas fa-envelope"></i> YMStourandtravel@gmail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> Arbaminch, Ethiopia</a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="hhttps://www.facebook.com/yorda.tibebu"><i class="fab fa-facebook-f"></i> Facebook</a>
+            <a href="https://www.twitter.com"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="https://www.instagram.com/yorda67/"><i class="fab fa-instagram"></i> Instagram</a>
+            <a href="#"> <i class="fab fa-linkedin"></i>Linkedin</a>
+         </div>
+
+      </div>
+
+      <div class="credit"> created by <span>YordaT</span> | all rights reserved! </div>
+
+   </section>
+
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
